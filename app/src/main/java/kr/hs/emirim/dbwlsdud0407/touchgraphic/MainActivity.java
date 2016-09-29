7f2c259e262ed91da850e566eddb2b168561e32b
Jinyoung Yu <appcreator08@AppCreator08ui-iMac.local>
2016-09-29 20:01:45 +0900

    #4 LinearLayout, handler

--- app/src/main/java/kr/hs/emirim/dbwlsdud0407/touchgraphic/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/dbwlsdud0407/touchgraphic/MainActivity.java
@@ -6,18 +6,34 @@ import android.graphics.Color;
 import android.graphics.Paint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutCompat;
 import android.view.View;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
-
+    DrawShape ds;
+    static final int LINE=1,RECT=2,CIRCLE=3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ds=new DrawShape(getApplicationContext());
        // setContentView(new DrawShape(getApplicationContext()));
+        LinearLayout linear=(LinearLayout)findViewById(R.id.linear_view);
+        linear.addView(ds);
     }
-    public void drawshape(View v){  //hander==view
 
+    public void drawshape(View v){  //hander==view
+        switch(v.getId()){
+
+            case R.id.but_line :
+                break;
+            case R.id.but_rect :
+                break;
+            case R.id.but_circle :
+                break;
+
+        }
 
 
 
